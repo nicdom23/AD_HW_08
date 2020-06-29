65de1a4a12637b11abacc8d3ac15da53b07af9a7
Nd23 <nicoladomenis23@hotmail.it>
2020-06-29 22:04:24 +0200

    almost over

--- Dikstra_Test/src/dijkstra.c
+++ Dikstra_Test/src/dijkstra.c
@@ -13,11 +13,14 @@ void init_SSSP(graph* G){
 }
 
 void relax(pqueue* Q,graph*G, int index_u, int index_v, int weight){
-    //printf("ff   %ld",G->V[index_u].d);
-    //printf("\nLL %ld  < %ld\n",G->V[index_u].d + weight,G->V[index_v].d);
+    
     if(G->V[index_u].d + weight < G->V[index_v].d ){
-        update_distance(Q,G,index_v, (G->V[index_u].d + weight));
+        node * new = (node*)malloc(sizeof(node));
+        new->d=(G->V[index_u].d + weight);
+        const void * new2= new; 
+        update_distance(Q,index_v, new2);
         G->V[index_u].pred = index_u;
+        free(new);
     }
 }
 
@@ -28,14 +31,13 @@ void dijkstra(graph* G, int index_s){
     pqueue * Q = build_pqueue(G->V,leq_dist,G->size_N,sizeof(node));
     
     while(!is_empty(Q)){
-        printf("NEXT");
+        
         int index_u = ext_min(Q);
 
-        //printf("\nnext %d \n",index_u);
-        //printf("lenght   %ld",G->V[index_u].d);
+       
         for (int index_v = 0; index_v<G->size_N;index_v++){
             if (G->WMat[index_u][index_v] >= 0){
-                //printf("\nDD  %d-%d\n",index_u,index_v);
+            
                 relax(Q,G,index_u,index_v,G->WMat[index_u][index_v]);
 
             }
